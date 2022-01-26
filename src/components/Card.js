523eb176d0f58dd7e9d107eb4252ae235fefb6fe
JaiveerS <jaiveer_@hotmail.com>
2022-01-26 15:41:44 -0500

    styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,9 +7,9 @@ export default function Card(prop){
             <img className="card--img" src={prop.card.imageUrl}></img>
             <div className="card--info">
                 <div className="card--cords">
-                    <h1 className="card--loc">loc</h1>
+                <i className="material-icons">add_location</i>
                     <h2 className="card--location">{prop.card.location}</h2>
-                    <a className="card-url"href={prop.card.googleMapsUrl}>View on Google Maps</a>
+                    <a className="card--url"href={prop.card.googleMapsUrl}>View on Google Maps</a>
                 </div>
                 <h1 className="card--title">{prop.card.title}</h1>
                 <p className="card--dates">{prop.card.startDate}-{prop.card.endDate}</p>
